da7090d01db735fb3e7fa57f475d3f0a58e1a33d
ABHISHEK SONI <86410022+abhi270794@users.noreply.github.com>
2022-01-03 18:58:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+  
+    <div className="App">
+    
+      <h1 > Recipe App</h1>
+      
+      <div className="addRecipe">
+      <h1>add recipe</h1>
+      <div className="button">
+       <button Onclick>add </button>
+       <div className="input1">
+      <input type="index" placeholder="enter" />
+      
+      </div>
+       </div>
+      </div>
+      <div className="Recipe">
+      <button>sort buttons</button>
+        <div className="recipe1">
+           <h1>Recipe 1</h1>
+        </div>
+        <div className="recipe2">
+           <h1>Recipe 2</h1>
+        </div>
+      </div>
+      <div className="RecipeInfo">
+      
+
+      </div>
+      
+    </div>
+   
+    </>
+  );
+}
+
+export default App;
